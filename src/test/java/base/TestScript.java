a63daee046570e3271cf26a8c020886cdeea7a6d
shark-2000 <katyayns40@gmail.com>
2022-07-06 23:08:00 +0530

    added testcase TC001 , TC002

--- src/test/java/base/TestScript.java
+++ src/test/java/base/TestScript.java
@@ -11,7 +11,7 @@ import pages.Users;
 import utils.ExtentReport;
 
 public class TestScript extends Report {
-	@Test(priority=0)
+	@Test(priority = 0)
 	public void prerequisite() throws IOException {
 		report = ExtentReport.init();
 		logger = report.startTest("ALM automation");
@@ -21,5 +21,29 @@ public class TestScript extends Report {
 		Prerequisite.login();
 	}
 
-	
+	@Test(dependsOnMethods = { "prerequisite" }, priority = 1)
+	public void TC_001() throws IOException {
+		DashBoard.clickOnUser();
+		Users.clickOnCreateUser();
+		Assert.assertEquals(CreateUser.pageTitle(), "Create User");
+
+	}
+
+	@Test(priority = 2)
+	public void TC_002() {
+		Assert.assertEquals(CreateUser.parentContainerTitle(), "User Details");
+		Assert.assertEquals(CreateUser.getFullName(), "Full Name");
+		Assert.assertEquals(CreateUser.getContact(), "Contact");
+		Assert.assertEquals(CreateUser.getEmail(), "Email");
+		Assert.assertEquals(CreateUser.getGender(), "Gender");
+		Assert.assertEquals(CreateUser.getJoiningDate(), "Joining Date");
+		Assert.assertEquals(CreateUser.getBirthDate(), "Birth Date");
+		Assert.assertEquals(CreateUser.getStatus(), "Status");
+
+	}
+
+	public void TC_003() {
+
+	}
+
 }
